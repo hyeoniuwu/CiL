9d5023b7325017beb461f922427b45a055c47ed7
hy <hy@hys.dev>
2025-09-10 08:23:11 +1000

    .

--- Computability/SetOracles.lean
+++ Computability/SetOracles.lean
@@ -747,6 +747,8 @@ theorem eval_clamped_prop': eval_clamped O u c x=Part.some y → eval O c x=Part
 theorem eval_clamped_prop''_aux (h:eval_clamped O u c x=Part.some y): (use O c x).Dom := e2u (Part.eq_some_imp_dom (eval_clamped_prop' h))
 -- theorem eval_clamped_prop''_aux' (h:y∈eval_clamped O u c x): (use O c x).Dom := e2u (Part.mem_imp_dom (eval_clamped_prop' h))
 theorem eval_clamped_prop'' (h:eval_clamped O u c x=Part.some y): y∈eval O c x ∧ (use O c x).get (eval_clamped_prop''_aux h)≤u := by sorry
+theorem eval_clamped_prop''_rev(h:(eval O c x).Dom) (h0:(use O c x).get (e2u h)≤u): eval_clamped O u c x=Part.some ((eval O c x).get h) := by sorry
+theorem eval_clamped_prop''_rev2: (use O c x).get h≤u ↔ (eval_clamped O u c x).Dom := by sorry
 -- def eval_string (σ:List ℕ) (c:Code) (x:ℕ):= eval_clamped (fun e=> σ.getD e 999) σ.length c x
 -- def eval_string (σ:List ℕ) (c:Code) (x:ℕ):= eval_clamped (fun e=> Nat.sg $ σ.getD e 999) σ.length c x
 def eval_string (σ:List ℕ) (c:Code) (x:ℕ):= eval_clamped (fun e=> b2n $ n2b $ σ.getD e 999) σ.length c x
@@ -1287,17 +1289,31 @@ let k:=(n2l (Bs (2*(i+1)-1))).length
   rw [i_1_simp]
   
 
-  unfold A at a1
-  unfold χ at a1
-  simp at a1
+  -- unfold A at a1
+  -- unfold χ at a1
+  -- simp at a1
   simp only [eval_string]
 
-  let compl := (eval (fun x ↦ if n2b ((As (x + 1))[x]'(AsSize)) = true then 1 else 0) (decodeCode i) k)
+  let compl := ((eval (χ A) (decodeCode i) k))
+  -- simp [show ((eval (χ A) (decodeCode i) k)) = compl from rfl] at a1
+  let usecomp := (use (χ A) (decodeCode i) k)
   have a2 := e2u a1
+  have : usecomp.Dom := by exact a2
+
   -- use reverse of eval_clamped_prop'' to rephrase the eval_clamped in the goal to just eval.
   -- then, use the extension that will get the oracle string to As (use).
   -- the inequality will be satisfies as the list as size greater than use.
-
+  have := eval_clamped_prop''_rev (a1) (Nat.le_refl ((usecomp).get (e2u a1)))
+  -- apply Exists.imp (λ x hx => 
+  -- match hx with
+  -- | ⟨h, h0⟩ => eval_clamped_prop''_rev2 h h0)
+  have := @eval_clamped_prop''_rev2 
+  #check Exists.imp
+  apply Exists.imp (eval_clamped_prop''_rev2)
+  · intro a
+  use 123
+  rw [eval_clamped_prop''_rev2]
+  simp [eval_clamped_prop''_rev2, -Denumerable.list_ofNat_succ]
 
   sorry
 theorem Aextends' : ¬(eval_string (As (2*i)) i k).Dom → ¬(evalSet A i k).Dom := by
