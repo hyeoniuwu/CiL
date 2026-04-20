5fdc2d0ff17959db5e747bf6e7f943a5dc5a8a26
hy <hy@hys.dev>
2026-04-21 00:04:02 +1000

    linter

--- lakefile.lean
+++ lakefile.lean
@@ -1,13 +1,40 @@
 import Lake
 open Lake DSL
 
+abbrev mathlibOnlyLinters : Array LeanOption := #[
+  ⟨`linter.mathlibStandardSet, true⟩,
+  -- Explicitly enable the header linter, since the standard set is defined in `Mathlib.Init`
+  -- but we want to run this linter in files imported by `Mathlib.Init`.
+  ⟨`linter.style.header, true⟩,
+  ⟨`linter.checkInitImports, true⟩,
+  ⟨`linter.allScriptsDocumented, true⟩,
+  ⟨`linter.pythonStyle, true⟩,
+  ⟨`linter.style.longFile, .ofNat 1500⟩,
+  -- ⟨`linter.nightlyRegressionSet, true⟩,
+  -- `latest_import.yml` uses this comment: if you edit it, make sure that the workflow still works
+]
+
+/-- These options are passed as `leanOptions` to building mathlib, as well as the
+`Archive` and `Counterexamples`. (`tests` omits the first two options.) -/
+abbrev mathlibLeanOptions := #[
+    ⟨`pp.unicode.fun, true⟩, -- pretty-prints `fun a ↦ b`
+    ⟨`autoImplicit, false⟩,
+    ⟨`maxSynthPendingDepth, .ofNat 3⟩,
+  ] ++ -- options that are used in `lake build`
+    mathlibOnlyLinters.map fun s ↦ { s with name := `weak ++ s.name }
+
+-- @[default_target]
+-- lean_lib «Computability» where
+--   leanOptions := Mathlib.mathlibLeanOptions
 package «computability» where
   -- Settings applied to both builds and interactive editing
-  leanOptions := #[
-    ⟨`autoImplicit, false⟩,
-    ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
-  ]
+  -- leanOptions := #[
+  --   ⟨`autoImplicit, false⟩,
+  --   ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
+  -- ]
+  leanOptions := mathlibLeanOptions
   -- add any additional package configuration options here
+  lintDriver := "batteries/runLinter"
 
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
