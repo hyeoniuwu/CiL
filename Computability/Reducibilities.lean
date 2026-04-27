aeb539b93903d380472c4ddc79308a358928e3e6
hy <hy@hys.dev>
2026-04-27 13:14:11 +1000

    prove reducibility

--- Computability/Reducibilities.lean
+++ Computability/Reducibilities.lean
@@ -14,8 +14,66 @@ import Mathlib.Computability.RecursiveIn
 In this file we define helper functions which will be used in later computability arguments.
 -/
 
-open Nat Oracle.Single
+open Nat Part Encodable
+section vars
+-- variable {α : Type*} {β : Type*} {σ : Type*} [Primcodable α] [Primcodable β] [Primcodable σ]
+variable {α : Type*} {β : Type*} {γ : Type*} {δ : Type*} {σ : Type*}
+variable [Primcodable α] [Primcodable β] [Primcodable γ] [Primcodable δ] [Primcodable σ]
+nonrec theorem RecursiveIn.comp {O} {f : β →. σ} {g : α → β} (hf : RecursiveIn O f) (hg : ComputableIn O g) :
+    RecursiveIn O fun a => f (g a) :=
+  (RecursiveIn.comp hf hg).of_eq fun n => by
+    simp only [PFun.coe_val, map_some, bind_eq_bind]
+    rcases e : decode (α := α) n with - | a <;> simp [encodek]
+  -- simpa [bind_some_eq_map] using RecursiveIn.bind (g := fun a x => some (g a x)) hf hg
+theorem RecursiveIn.nat_iff {O} {f : ℕ →. ℕ} : _root_.RecursiveIn O f ↔ Nat.RecursiveIn O f := by
+  simp [_root_.RecursiveIn, map_id']
+theorem unpaired {O} {f : ℕ → ℕ →. α} : RecursiveIn O (Nat.unpaired f) ↔ RecursiveIn₂ O f :=
+  -- sorry
+  ⟨fun h => by simpa using RecursiveIn.comp (g := fun p : ℕ × ℕ => (p.1, p.2)) h Primrec₂.pair.to_comp.computableIn,
+    fun h => h.comp Primrec.unpair.to_comp.computableIn⟩
+theorem RecursiveIn₂.unpaired' {O} {f : ℕ → ℕ →. ℕ} : Nat.RecursiveIn O (Nat.unpaired f) ↔ RecursiveIn₂ O f :=
+  RecursiveIn.nat_iff.symm.trans unpaired
+protected theorem Nat.RecursiveIn.some {O} : Nat.RecursiveIn O some :=
+  (Nat.Partrec.of_primrec Primrec.id).recursiveIn
+protected theorem RecursiveIn.bind {O} {f : α →. β} {g : α → β →. σ} (hf : RecursiveIn O f) (hg : RecursiveIn₂ O g) :
+    RecursiveIn O fun a => (f a).bind (g a) :=
+  (Nat.RecursiveIn.comp hg (Nat.RecursiveIn.some.pair hf)).of_eq fun n => by
+    rcases e : decode (α := α) n <;> simp [Seq.seq, e, encodek]
+theorem RecursiveIn.map {O} {f : α →. β} {g : α → β → σ} (hf : RecursiveIn O f) (hg : ComputableIn₂ O g) :
+    RecursiveIn O fun a => (f a).map (g a) := by
+  simpa [bind_some_eq_map] using RecursiveIn.bind (g := fun a x => some (g a x)) hf hg
+  -- sorry
+end vars
 
+theorem rfind' {O f} (hf : Nat.RecursiveIn O f) :
+    Nat.RecursiveIn O
+      (Nat.unpaired fun a m =>
+        (Nat.rfind fun n => (fun m => m = 0) <$> f (Nat.pair a (n + m))).map (· + m)) :=
+  RecursiveIn₂.unpaired'.2 <| by
+    refine
+      RecursiveIn.map
+        ((@RecursiveIn₂.unpaired' O fun a b : ℕ =>
+              Nat.rfind fun n => (fun m => m = 0) <$> f (Nat.pair a (n + b))).1
+          ?_)
+        -- (Primrec.nat_add.comp Primrec.snd <| Primrec.snd.comp Primrec.fst).to_comp.to₂
+        (Primrec.nat_add.comp Primrec.snd <| Primrec.snd.comp Primrec.fst).to₂.computableIn₂
+    have : Nat.RecursiveIn O (fun a => Nat.rfind (fun n => (fun m => decide (m = 0)) <$>
+      Nat.unpaired (fun a b => f (Nat.pair (Nat.unpair a).1 (b + (Nat.unpair a).2)))
+        (Nat.pair a n))) :=
+        by
+        refine Nat.RecursiveIn.rfind ?_
+        refine RecursiveIn₂.unpaired'.2 ?_
+        apply RecursiveIn.comp
+        · exact RecursiveIn.nat_iff.mpr hf
+
+        have a0 := (Primrec₂.pair.comp (Primrec.fst.comp <| Primrec.unpair.comp Primrec.fst)
+                  (Primrec.nat_add.comp Primrec.snd
+                    (Primrec.snd.comp <| Primrec.unpair.comp Primrec.fst))).to_comp
+        -- have a1 := @Computable.computableIn _ _ _ _ _ O a0
+        exact Computable.computableIn a0
+    simpa
+
+open Oracle.Single
 theorem SingleReducibleIff (O : ℕ → ℕ) (f : ℕ →. ℕ) :
     Oracle.Single.RecursiveIn O f ↔ Nat.RecursiveIn ({↑O}:(Set (ℕ →. ℕ))) f := by
   constructor
@@ -29,7 +87,7 @@ theorem SingleReducibleIff (O : ℕ → ℕ) (f : ℕ →. ℕ) :
     | pair hf hh hf_ih hh_ih => exact Nat.RecursiveIn.pair hf_ih hh_ih
     | comp hf hh hf_ih hh_ih => exact Nat.RecursiveIn.comp hf_ih hh_ih
     | prec hf hh hf_ih hh_ih => exact Nat.RecursiveIn.prec hf_ih hh_ih
-    | rfind hf ih => sorry
+    | rfind hf ih => exact rfind' ih
   · intro h
     induction h with
     | zero => exact Oracle.Single.RecursiveIn.zero
